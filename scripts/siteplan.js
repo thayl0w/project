1578d25ba7506eb0b32611a05a53209306ed183a
thayl0w <softmardevelopment@gmail.com>
2024-12-05 16:55:19 +0800

    changes

---
+++ scripts/siteplan.js
@@ -0,0 +1,10 @@
+const currentYear = new Date().getFullYear();
+
+
+document.getElementById("currentyear").textContent = currentYear;
+
+
+const lastModifiedDate = document.lastModified;
+
+
+document.getElementById("lastModified").textContent = "Last Modified: " + lastModifiedDate;
